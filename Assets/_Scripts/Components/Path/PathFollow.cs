8f088b82d2a063d222a84be5efc860b9c89b6587
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-10-23 17:18:13 +0100

    feat: path follower

---
+++ Assets/_Scripts/Components/Path/PathFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PathFollow : MonoBehaviour
+{
+    [SerializeField] private Path _path;
+    [SerializeField] private float _speed;
+    private int _currentPointIndex = 0;
+
+    public System.Action OnPathFinished { get; set; } 
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            _path.Points[_currentPointIndex].position,
+            _speed * Time.deltaTime
+        );
+
+        if (float.Epsilon < Vector2.Distance(
+            transform.position, _path.Points[_currentPointIndex].position
+        ))
+            return;
+
+        _currentPointIndex++;
+
+        if (_currentPointIndex == _path.Points.Length)
+        {
+            enabled = false;
+            OnPathFinished?.Invoke();
+        }
+    }
+}
