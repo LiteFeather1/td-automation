2c0375d3a32cab7f533b41a181f3b8ba983db16f
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-10-24 21:58:02 +0100

    feat: added OnDied and OnPathReached to enemies

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -1,7 +1,38 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private float _damage;
+
+    [Header("References")]
     [SerializeField] private Health _health;
     [SerializeField] private PathFollow _pathFollow;
+
+    public float Damage => _damage;
+
+    public Action<Enemy> OnDied { get; set; }
+    public Action<Enemy> OnPathReached { get; set; }
+
+    private void OnEnable()
+    {
+        _health.OnDied += Died;
+        _pathFollow.OnPathFinished += PathFinished;
+    }
+
+    private void OnDisable()
+    {
+        _health.OnDied -= Died;
+        _pathFollow.OnPathFinished -= PathFinished;
+    }
+
+    private void Died()
+    {
+        OnDied?.Invoke(this);
+    }
+
+    private void PathFinished()
+    {
+        OnPathReached?.Invoke(this);
+    }
 }
