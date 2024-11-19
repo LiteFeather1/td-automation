4a83808f83f53751e9b9d0616bc3f2f208e2875f
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-11-19 12:06:29 +0000

    feat: UI Resource

---
+++ Assets/_Scripts/UI/UIResource.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIResource : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI t_count;
+    [SerializeField] private Image i_icon;
+    [SerializeField] private Color _fadedColor = Color.white;
+
+    public void SetCount(int count)
+    {
+        if (count == 0)
+        {
+            i_icon.color = _fadedColor;
+            t_count.text = "";
+            return;
+        }
+
+        if (i_icon.color.a != 1f)
+            i_icon.color = Color.white;
+
+        t_count.text = count.ToString();
+    }
+}
