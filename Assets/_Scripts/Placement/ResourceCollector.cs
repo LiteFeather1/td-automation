752f3afebf91231e3665f01769de51c31a3396d0
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-11-21 20:05:40 +0000

    feat: resource collector

--- Assets/_Scripts/Placement/ResourceCollector.cs
+++ Assets/_Scripts/Placement/ResourceCollector.cs
@@ -1,14 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceCollector : Building, IOutPort
 {
-    [SerializeField] private float _timeToCollect = 1f;
-    [SerializeField] private ResourceBehaviour _resourceToGive;
-    private Direction _outDirection = Direction.Right;
+    [SerializeField] private ResourceType _type;
+    [SerializeField] private float _range = 5f;
+    [SerializeField] private float _timeToCollect = 5f;
+    [SerializeField] private float _speedPerNode = 2f;
     private float _elapsedTime = 0f;
 
+    private readonly List<ResourceNode> r_resourceNodes = new();
+
     public IInPort Port { get; set; }
-    public Direction OutDirection { get => _outDirection; set => _outDirection = value; }
+    public Direction OutDirection { get; set; } = Direction.Right;
 
     public override bool CanBeRotated => true;
     public override bool CanBeDestroyed => true;
@@ -18,13 +22,49 @@ public class ResourceCollector : Building, IOutPort
         if (Port == null || !Port.CanReceiveResource)
             return;
 
-        _elapsedTime += Time.deltaTime;
+        _elapsedTime += Time.deltaTime * _speedPerNode * r_resourceNodes.Count;
         if (_elapsedTime > _timeToCollect)
         {
             _elapsedTime %= _timeToCollect;
             Port.GiveResource(Instantiate(
-                _resourceToGive, new(Port.Position.x, Port.Position.y), Quaternion.identity
+                r_resourceNodes[Random.Range(0, r_resourceNodes.Count)].CollectResource(),
+                new(Port.Position.x, Port.Position.y),
+                Quaternion.identity
             ));
         }
+    }
+
+    public void OnDisable()
+    {
+        foreach (var node in r_resourceNodes)
+        {
+            node.OnDepleted -= OnNodeDepleted;
+        }
+    }
+
+    public void SetResources(List<ResourceNode> resourceNodes)
+    {
+        foreach (var node in resourceNodes)
+        {
+            if (node.Type == _type && Vector2Int.Distance(Position, node.Position) < _range)
+            {
+                r_resourceNodes.Add(node);
+                node.OnDepleted += OnNodeDepleted;
+            }
+        }
+
+        enabled = r_resourceNodes.Count > 0;
+    }
+
+    private void OnNodeDepleted(ResourceNode node)
+    {
+        r_resourceNodes.Remove(node);
+        node.OnDepleted -= OnNodeDepleted;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _range);
     }
 }
