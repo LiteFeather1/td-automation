bbfdeef48f1752842839ef8d9f11899e3ba27a62
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-11-24 15:48:52 +0000

    feat: UIBuildingButtons now has events when hovered

--- Assets/_Scripts/UI/UIBuildingButton.cs
+++ Assets/_Scripts/UI/UIBuildingButton.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UIBuildingButton : MonoBehaviour
+public class UIBuildingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private PlaceableData _placeableData;
     [SerializeField] private Image _image;
     [SerializeField] private Button _button;
 
     public Action<PlaceableData> OnButtonPressed { get; set; }
+    public Action<UIBuildingButton> OnButtonHovered { get; set; }
+    public Action OnButtonUnhovered { get; set; }
 
-    // TODO Remove This
     public PlaceableData PlaceableData => _placeableData;
     public Dictionary<ResourceType, int> ResourceCost => _placeableData.BuildingPrefab.ResourceCost;
 
     public void B_ButtonPressed()
     {
         OnButtonPressed?.Invoke(_placeableData);
+    }
+
+    public void OnPointerEnter(PointerEventData _)
+    {
+        OnButtonHovered?.Invoke(this);
+    }
+
+    public void OnPointerExit(PointerEventData _)
+    {
+        OnButtonUnhovered?.Invoke();
     }
 
     public void SetButtonInteractable(bool interactable)
