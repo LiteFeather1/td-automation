b506180df3ab2ef52a23673d05f59ae7386bed72
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-11-25 19:54:36 +0000

    feat: ObjectPool

---
+++ Assets/_Scripts/Object_Pool/Editor/ObjectPoolDrawer.cs
@@ -0,0 +1,42 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ObjectPool<>))]
+public class WeightedObjectDrawer : PropertyDrawer
+{
+    private const float PAD = 32f;
+    private const string TOOLTIP = "Second Parameter is the Initial pool size";
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        using var p = property.FindPropertyRelative("_object");
+        return EditorGUI.GetPropertyHeight(p, label);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        using var objectP = property.FindPropertyRelative("_object");
+        using var initialSizeP = property.FindPropertyRelative("_initialPoolSize");
+        label.tooltip = TOOLTIP;
+
+        using (new EditorGUI.PropertyScope(position, label, property))
+        {
+            position.width -= PAD;
+            EditorGUI.PropertyField(position, objectP, label, true);
+
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            position.x += PAD + position.width;
+            position.width = PAD - 4;
+            position.height = EditorGUI.GetPropertyHeight(initialSizeP);
+            position.x -= position.width;
+
+            EditorGUI.PropertyField(position, initialSizeP, GUIContent.none);
+
+            EditorGUI.indentLevel = indent;
+        }
+    }
+}
+#endif
---
+++ Assets/_Scripts/Object_Pool/ObjectPool.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ObjectPool<T> : IDisposable where T : Component
+{
+    [SerializeField] private T _object;
+    [SerializeField] private int _initialPoolSize;
+    private Transform _poolParent;
+    private readonly Queue<T> _inactiveObjects = new();
+
+    public HashSet<T> Objects { get; private set; } = new();
+
+    public Action<T> ObjectCreated { get; set; }
+
+    public T Object => _object;
+    public Transform PoolParent => _poolParent;
+
+    public ObjectPool(T object_, int initialPoolSize)
+    {
+        _object = object_;
+        _initialPoolSize = initialPoolSize;
+    }
+
+    public ObjectPool(T object_) : this(object_, 10) { }
+
+    public ObjectPool(int initialPoolSize) : this(null, initialPoolSize) { }
+
+    public ObjectPool() { }
+
+    ~ObjectPool()
+    {
+        Dispose();
+    }
+
+    public void InitPool(int size, bool spawnActive = false)
+    {
+        if (_poolParent != null)
+            return;
+
+        var name = _object.name;
+
+        _poolParent = new GameObject($"Pool_{name}").transform;
+
+        _object = UnityEngine.Object.Instantiate(_object);
+        _object.gameObject.SetActive(spawnActive);
+        _object.name = name;
+        _object.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        for (int i = 0; i < size; i++)
+            _inactiveObjects.Enqueue(Instantiate());
+    }
+
+    public void InitPool(bool spawnActive = false) => InitPool(_initialPoolSize, spawnActive);  
+
+    public T GetObject()
+    {
+        if (_inactiveObjects.Count > 0)
+            return _inactiveObjects.Dequeue();
+
+        return Instantiate();
+    }
+
+    public void ReturnObject(T object_)
+    {
+        _inactiveObjects.Enqueue(object_);
+    }
+
+    public void Dispose()
+    {
+        Objects.Clear();
+        UnityEngine.Object.Destroy(_poolParent.gameObject);
+    }
+
+    private T Instantiate()
+    {
+        T object_ = UnityEngine.Object.Instantiate(_object, _poolParent);
+        object_.name = $"{_object.name}_{Objects.Count}";
+        Objects.Add(object_);
+        ObjectCreated?.Invoke(object_);
+        return object_;
+    }
+}
