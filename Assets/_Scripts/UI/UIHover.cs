a4f034d0dc2e75a3aacdea0a31cd6be4d4cd0097
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-12-02 16:44:37 +0000

    feat: UIHover

---
+++ Assets/_Scripts/UI/UIHover.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, TextArea] private string _text;
+
+    public Action<string> OnHover { get; set; }
+    public Action OnUnhover { get; set; }
+
+    public void OnPointerEnter(PointerEventData _)
+    {
+        OnHover?.Invoke(_text);
+    }
+
+    public void OnPointerExit(PointerEventData _)
+    {
+        OnUnhover?.Invoke();
+    }
+}
