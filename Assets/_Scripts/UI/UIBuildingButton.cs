63c3ce0fe5cd1df2f3255dffd57ea2a6ecb00717
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-12-02 16:28:51 +0000

    feat: add input action to call a button press with input

--- Assets/_Scripts/UI/UIBuildingButton.cs
+++ Assets/_Scripts/UI/UIBuildingButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class UIBuildingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -10,12 +11,26 @@ public class UIBuildingButton : MonoBehaviour, IPointerEnterHandler, IPointerExi
     [SerializeField] private Image _image;
     [SerializeField] private Button _button;
 
+    [SerializeField] private InputAction _pressInputAction;
+
     public Action<PlaceableData> OnButtonPressed { get; set; }
     public Action<UIBuildingButton> OnButtonHovered { get; set; }
     public Action OnButtonUnhovered { get; set; }
 
     public PlaceableData PlaceableData => _placeableData;
     public Dictionary<ResourceType, int> ResourceCost => _placeableData.BuildingPrefab.ResourceCost;
+
+    internal void OnEnable()
+    {
+        _pressInputAction.performed += PressInput;
+        _pressInputAction.Enable();
+    }
+
+    internal void OnDisable()
+    {
+        _pressInputAction.performed -= PressInput;
+        _pressInputAction.Disable();
+    }
 
     public void B_ButtonPressed()
     {
@@ -35,5 +50,10 @@ public class UIBuildingButton : MonoBehaviour, IPointerEnterHandler, IPointerExi
     public void SetButtonInteractable(bool interactable)
     {
         _button.interactable = interactable;
+    }
+
+    private void PressInput(InputAction.CallbackContext ctx)
+    {
+        OnButtonPressed?.Invoke(_placeableData);
     }
 }
