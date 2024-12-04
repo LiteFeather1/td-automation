fdc08f8cd6d59ad68b8c65ddbae26cf9e57be99f
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-12-04 11:45:49 +0000

    fix: hit flash now resets shader on death

--- Assets/_Scripts/Misc/HitFlash.cs
+++ Assets/_Scripts/Misc/HitFlash.cs
@@ -3,22 +3,29 @@ using UnityEngine;
 
 public class HitFlash : MonoBehaviour
 {
+    private const string INTENSITY = "_Intensity";
+
     [SerializeField] private SpriteRenderer _sr;
 
     private readonly YieldInstruction r_waitFlash = new WaitForSeconds(.125f);
+
+    private void OnDisable()
+    {
+        _sr.material.SetFloat(INTENSITY, 0f);
+    }
 
     [ContextMenu("Test")]
     public void DoFlash()
     {
         StopAllCoroutines();
-        _sr.material.SetFloat("_Intensity", 0f);
+        _sr.material.SetFloat(INTENSITY, 0f);
         StartCoroutine(FlashAnimation());
     }
 
     private IEnumerator FlashAnimation()
     {
-        _sr.material.SetFloat("_Intensity", 1f);
+        _sr.material.SetFloat(INTENSITY, 1f);
         yield return r_waitFlash;
-        _sr.material.SetFloat("_Intensity", 0f);
+        _sr.material.SetFloat(INTENSITY, 0f);
     }
 }
