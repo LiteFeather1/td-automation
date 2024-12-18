cd44e71109d86ddb856c865054c9d999ead3e6cd
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-12-18 22:11:17 +0000

    feat: SceneNamesIDs

---
+++ Assets/_Scripts/Managers/SceneNamesIDs.cs
@@ -0,0 +1,8 @@
+public static class SceneNamesIDs
+{
+    public const string MAIN_MENU = "MainMenu";
+    public const string MAIN = "Main";
+
+    public const int MAIN_MENU_ID = 0;
+    public const int MAIN_ID = 1;
+}
