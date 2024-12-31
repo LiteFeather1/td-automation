d7266648c9f9565bbaab4f7def66345684092e4b
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-12-31 17:40:32 +0000

    feat: flamethrower playing when damaging enemies

--- Assets/_Scripts/Tower/TowerFlamethrower.cs
+++ Assets/_Scripts/Tower/TowerFlamethrower.cs
@@ -5,9 +5,12 @@ public class TowerFlamethrower : Tower
     [Header("Flamethrower tower")]
     [SerializeField] private float _fireDamageRadius = 2f;
     [SerializeField, Range(0f, 1f)] private float _fireDamagePercetage = .33f;
+    [SerializeField] private ParticleSystem _ps;
 
     protected override void DamageEnemy(Enemy enemy)
     {
+        _ps.Play();
+
         foreach (Enemy e in EnemyManager.Instance.Enemies)
         {
             if (Vector2.Distance(enemy.transform.position, e.transform.position) > _fireDamageRadius)
