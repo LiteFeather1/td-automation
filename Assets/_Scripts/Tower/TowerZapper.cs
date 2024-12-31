39ad50696bcb14888cb763689aabd2737d6fa6fc
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-12-31 21:17:21 +0000

    feat: zapper triggering zap effect

--- Assets/_Scripts/Tower/TowerZapper.cs
+++ Assets/_Scripts/Tower/TowerZapper.cs
@@ -1,34 +1,43 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerZapper : Tower
 {
     [Header("Zapper")]
     [SerializeField] private float _multiplierPerEnemyInRange = .25f;
-    
+
     protected override void Update()
     {
         if (!EnemyManager.Instance.HasEnemies)
             return;
 
-        List<Enemy> enemiesInRange = new();
+        int enemiesInRange = 0;
         foreach (Enemy enemy in EnemyManager.Instance.Enemies)
         {
             if (Vector2.Distance(enemy.transform.position, transform.position) < Range)
-                enemiesInRange.Add(enemy);
+                enemiesInRange++;
         }
 
-        if (enemiesInRange.Count == 0)
+        if (enemiesInRange == 0)
             return;
 
-        _elapsedTime += Time.deltaTime * (1f + (_multiplierPerEnemyInRange * enemiesInRange.Count));
+        _elapsedTime += Time.deltaTime * (1f + (_multiplierPerEnemyInRange * enemiesInRange));
         if (_elapsedTime < _damageRate)
             return;
 
-        foreach (Enemy enemy in enemiesInRange)
-            DamageEnemy(enemy);
-
         _elapsedTime %= _damageRate;
+
+        Vector2[] positions = new Vector2[enemiesInRange];
+        for (int i = 0; i < EnemyManager.Instance.Enemies.Count; i++)
+        {
+            Enemy enemy = EnemyManager.Instance.Enemies[i];
+            if (Vector2.Distance(enemy.transform.transform.position, transform.position) > Range)
+                continue;
+
+            DamageEnemy(enemy);
+            positions[i] = enemy.transform.position;
+        }
+
+        ZapEffectManager.Instance.ZapEffect(transform.position, positions);
     }
 
     protected override void DamageEnemy(Enemy enemy)
